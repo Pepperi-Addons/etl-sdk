dd95ddd0b51b47a19d704a081af170a51e441b22
Saar Guttman <saar.g@pepperi.com>
2023-06-08 14:35:59 +0300

    Refactor properties to functions

--- entities/build-operations.ts
+++ entities/build-operations.ts
@@ -1,5 +1,6 @@
-export interface BuildOperations<T,K,L> {
-	getObjectsByPage: (page: number, pageSize: number, additionalFields?: string) => Promise<T[]>,
-	fixObjects: (objects: T[]) => K[],
-	batchUpsert: (resourceName: string, objects: K[]) => Promise<L[]>
+export interface BuildOperations<T,K,L>
+{
+	getObjectsByPage (page: number, pageSize: number, additionalFields?: string): Promise<T[]>,
+	fixObjects (objects: T[]): K[],
+	batchUpsert (resourceName: string, objects: K[]): Promise<L[]>
 }
